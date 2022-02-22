6596454df421c11e24db50e0ed78bfc208721d60
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-02-22 23:59:46 +0900

    crop

--- ios/Classes/QrReader.swift
+++ ios/Classes/QrReader.swift
@@ -158,6 +158,7 @@ class QrReader: NSObject {
     
     captureSession.addInput(input)
     captureSession.addOutput(output)
+    captureSession.sessionPreset = .hd1920x1080
   }
   
   func start() {
@@ -188,12 +189,16 @@ extension QrReader: AVCaptureVideoDataOutputSampleBufferDelegate {
     
     pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
     textureRegistry.textureFrameAvailable(self.textureId)
- 
+
+    let rect: CGRect = CGRect(x: 0, y:0, width: 1920/2, height: 1080)
+
+    let croppedImage = croppedSampleBuffer(sampleBuffer, with: rect)
+
     guard !isProcessing.swap(true) else {
       return
     }
     
-    let image = VisionImage(buffer: sampleBuffer)
+    let image = VisionImage(buffer: croppedImage!)
     image.orientation = imageOrientation(
       deviceOrientation: UIDevice.current.orientation,
       defaultOrientation: .portrait
@@ -247,5 +252,101 @@ extension QrReader: AVCaptureVideoDataOutputSampleBufferDelegate {
         return imageOrientation(deviceOrientation: defaultOrientation, defaultOrientation: .portrait)
         }
     }
+
+
+  func croppedSampleBuffer(_ sampleBuffer: CMSampleBuffer,
+                           with rect: CGRect) -> CMSampleBuffer? {
+    guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+
+    CVPixelBufferLockBaseAddress(imageBuffer, .readOnly)
+
+    let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+    let width = CVPixelBufferGetWidth(imageBuffer)
+    let height = CVPixelBufferGetHeight(imageBuffer)
+    let bytesPerPixel = bytesPerRow / width
+    guard let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer) else { return nil }
+    let baseAddressStart = baseAddress.assumingMemoryBound(to: UInt8.self)
+
+    var cropX = Int(rect.origin.x)
+    let cropY = Int(rect.origin.y)
+
+    // Start pixel in RGB color space can't be odd.
+    if cropX % 2 != 0 {
+      cropX += 1
+
+    }
+
+    let cropStartOffset = Int(cropY * bytesPerRow + cropX * bytesPerPixel)
+
+    var pixelBuffer: CVPixelBuffer!
+    var error: CVReturn
+
+    // Initiates pixelBuffer.
+    let pixelFormat = CVPixelBufferGetPixelFormatType(imageBuffer)
+    let options = [
+      kCVPixelBufferCGImageCompatibilityKey: true,
+      kCVPixelBufferCGBitmapContextCompatibilityKey: true,
+      kCVPixelBufferWidthKey: rect.size.width,
+      kCVPixelBufferHeightKey: rect.size.height
+    ] as [CFString : Any]
+
+    error = CVPixelBufferCreateWithBytes(kCFAllocatorDefault,
+            Int(rect.size.width),
+            Int(rect.size.height),
+            pixelFormat,
+            &baseAddressStart[cropStartOffset],
+            Int(bytesPerRow),
+            nil,
+            nil,
+            options as CFDictionary,
+            &pixelBuffer)
+    if error != kCVReturnSuccess {
+      print("Crop CVPixelBufferCreateWithBytes error \(Int(error))")
+      return nil
+    }
+
+
+    // Cropping using CIImage.
+    var ciImage = CIImage(cvImageBuffer: imageBuffer)
+    ciImage = ciImage.cropped(to: rect)
+    // CIImage is not in the original point after cropping. So we need to pan.
+    ciImage = ciImage.transformed(by: CGAffineTransform(translationX: CGFloat(-cropX), y: CGFloat(-cropY)))
+
+//    let ciContext = CIContext(options: nil)
+//    ciContext.render(ciImage, to: pixelBuffer!)
+//    gCIContext.render(ciImage, to: pixelBuffer!)
+
+    // Prepares sample timing info.
+    var sampleTime = CMSampleTimingInfo()
+    sampleTime.duration = CMSampleBufferGetDuration(sampleBuffer)
+    sampleTime.presentationTimeStamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+    sampleTime.decodeTimeStamp = CMSampleBufferGetDecodeTimeStamp(sampleBuffer)
+
+    var videoInfo: CMVideoFormatDescription!
+    error = CMVideoFormatDescriptionCreateForImageBuffer(allocator: kCFAllocatorDefault,
+            imageBuffer: pixelBuffer, formatDescriptionOut: &videoInfo)
+    if error != kCVReturnSuccess {
+      print("CMVideoFormatDescriptionCreateForImageBuffer error \(Int(error))")
+      CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags.readOnly)
+      return nil
+    }
+
+
+    // Creates `CMSampleBufferRef`.
+    var resultBuffer: CMSampleBuffer?
+    error = CMSampleBufferCreateForImageBuffer(allocator: kCFAllocatorDefault,
+            imageBuffer: pixelBuffer,
+            dataReady: true,
+            makeDataReadyCallback: nil,
+            refcon: nil,
+            formatDescription: videoInfo,
+            sampleTiming: &sampleTime,
+            sampleBufferOut: &resultBuffer)
+    if error != kCVReturnSuccess {
+      print("CMSampleBufferCreateForImageBuffer error \(Int(error))")
+    }
+    CVPixelBufferUnlockBaseAddress(imageBuffer, .readOnly)
+    return resultBuffer
+  }
 
 }
